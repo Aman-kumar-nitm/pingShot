c409d0e20de8ccec7213909a83b902648da60925
Aman-kumar-nitm <amankumarec2023nitm@gmail.com>
2025-09-18 18:39:37 +0530

    second-change

--- chat-application/src/App.jsx
+++ chat-application/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 const {authUser}=useContext(AuthContext);
 console.log(authUser);
   return (
-    <div className="bg-[url('./src/chat-app-assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster/>
         <Routes>
         <Route path='/' element={authUser?<Home/>:<Navigate to='/login'/>} />
